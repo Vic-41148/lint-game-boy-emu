06d72272b0abb4cfa09edaa9c0ad35522214e55f
Vic-41148 <zorozamarimo@gmail.com>
2026-01-05 03:24:41 +0530

    8bit reader done

--- bus.cpp
+++ bus.cpp
@@ -20,3 +20,57 @@ bus::bus()
     high_ram_.resize(0x7F);                          //127  bytes
     interrupt_enable_register_=0x00;                   //1.00 bytes
 }
+
+//reader
+uint8_t bus::read8(uint32_t address)
+{
+    if(address<=0x3FFF && address>=0x0000)
+    {
+        rom_bank_00_[address];
+    }
+    else if(address<=0x7FFF && address>=0x4000)
+    {
+        rom_bank_01_nn_[address];
+    }
+    else if(address<=0x9FFF && address>=0x8000)
+    {
+        rom_bank_01_nn_[address];
+    }
+    else if(address<=0xBFFF && address>=0xA000)
+    {
+        rom_bank_01_nn_[address];
+    }
+    else if(address<=0xCFFF && address>=0xC000)
+    {
+        rom_bank_01_nn_[address];
+    }
+    else if(address<=0xDFFF && address>=0xD000)
+    {
+        rom_bank_01_nn_[address];
+    }
+    else if(address<=0xFDFF && address>=0xE000)
+    {
+        rom_bank_01_nn_[address];
+    }
+    else if(address<=0xFE9F && address>=0xFE00)
+    {
+        rom_bank_01_nn_[address];
+    }
+    else if(address<=0xFEFF && address>=0xFEA0)
+    {
+        rom_bank_01_nn_[address];
+    }
+    else if(address<=0xFF7F && address>=0xFF00)
+    {
+        rom_bank_01_nn_[address];
+    }
+    else if(address<=0xFFFE && address>=0xFF80)
+    {
+        rom_bank_01_nn_[address];
+    }
+    else if(address==0xFFFF)
+    {
+        return interrupt_enable_register_;
+    }
+}
+
